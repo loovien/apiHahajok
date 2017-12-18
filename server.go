98e3fa9fede999496182a9f34bd121ba63949b6d
luowen <bigpao.luo@gmail.com>
2017-12-18 10:38:42 +0800

    support graceful shutdown

--- server.go
+++ server.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/vvotm/apiHahajok/boot"
+import (
+	"github.com/vvotm/apiHahajok/boot"
+	"os/signal"
+	"os"
+)
 
 func main() {
 	app := boot.GetApp().AppInst
-	app.Logger.Errorf("系统错误: %v", app.Start(":1223"))
+	go func() {
+		app.Logger.Errorf("服务停止鸟: %v", app.Start(":1223"))
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+	signalNum := <- sigChan
+	app.Logger.Infof("接受系统信号: %v", signalNum)
 }
